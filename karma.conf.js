0f4282f83d8f1efe398fbb8b85156486d581d339
Amit Portnoy <amit.portnoy@gmail.com>
2016-03-16 10:23:20 +0200

    test with firefox on travis

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome', 'Firefox'],
+
+    frameworks: ['browserify', 'mocha'],
+
+    files: ['test/**/*.js'],
+    preprocessors: {
+      'test/**/*.js': [ 'browserify' ]
+    },
+
+    singleRun: true,
+    autoWatch: false,
+
+    browserify: {
+      transform: [ 'babelify' ]
+    }
+  });
+};
