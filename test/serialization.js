93c53af03be7059a1e3e96be67e7095d2c5347e3
amitportnoy <amitportnoy@dalet.com>
2016-12-22 11:49:37 +0200

    add skeleton serialization tests

--- test/serialization.js
+++ test/serialization.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const sinon = require('sinon');
 
 const {Serializer, Deserializer} = require('../lib/serialization');
+const skeleton = require('../lib/skeleton');
 
 describe('serialization', () => {
   let serializer;
@@ -10,81 +11,88 @@ describe('serialization', () => {
     serializer = new Serializer();
   });
 
-  describe('regular deserializer', () => {
-    let deserializer;
+  [skeleton, function noOp(_) {return _;}].forEach((sourceMapper) => {
+    describe(`serialization after ${sourceMapper.name}`, () => {
+      describe('regular deserializer', () => {
+        let deserializer;
 
-    beforeEach(() => {
-      deserializer = new Deserializer();
-    });
+        beforeEach(() => {
+          deserializer = new Deserializer();
+        });
 
-    it('is deep equal', () => {
-      const source = {p: 1, o: {arr: [{p: 1}]}};
-      const target = deserializer.deserialize(serializer.serialize(source));
+        it('is deep equal', () => {
+          const source = sourceMapper({p: 1, o: {arr: [{p: 1}]}});
+          const target = deserializer.deserialize(serializer.serialize(source));
 
-      assert.deepEqual(target, source);
-    });
+          assert.deepEqual(target, source);
+        });
 
-    it('preserves references', () => {
-      const o = {};
-      const target = deserializer.deserialize(serializer.serialize({o1: o, o2: o}));
+        it('preserves references', () => {
+          const o = {};
+          const target = deserializer.deserialize(serializer.serialize(sourceMapper({o1: o, o2: o})));
 
-      assert.strictEqual(target.o1, target.o2);
-    });
+          assert.strictEqual(target.o1, target.o2);
+        });
 
-    it('preserves reference loop', () => {
-      const source = {};
-      source.loop = source;
-      const target = deserializer.deserialize(serializer.serialize(source));
+        it('preserves reference loop', () => {
+          const source = sourceMapper({});
+          source.loop = source;
+          const target = deserializer.deserialize(serializer.serialize(source));
 
-      assert.strictEqual(target.loop, target);
-    });
+          assert.strictEqual(target.loop, target);
+        });
 
-    it('respects inner clone method', () => {
-      const target = deserializer.deserialize(serializer.serialize({
-        o: {
-          clone() {
-            return {n: 3};
-          }
+        it('respects inner clone method', () => {
+          const target = deserializer.deserialize(serializer.serialize(sourceMapper({
+            o: {
+              clone() {
+                return {n: 3};
+              }
+            }
+          })));
+
+          assert.deepEqual(target.o.n, 3);
+        });
+
+        it('removes a property when it\'s value clone method returns undefined', () => {
+          const target = deserializer.deserialize(serializer.serialize(sourceMapper({
+            o: {
+              clone() {
+              }
+            }
+          })));
+
+          assert(!target.hasOwnProperty('o'));
+        });
+
+        it('inner clone method receives serialize\'s second parameter', () => {
+          const clone = sinon.spy();
+
+          deserializer.deserialize(serializer.serialize(sourceMapper({
+            o: {clone}
+          }), 1));
+
+          assert(clone.calledOnce);
+          assert(clone.calledWithExactly(1));
+        });
+      });
+
+      it('deserializer with extra classes', () => {
+        class C1 {
         }
-      }));
-
-      assert.deepEqual(target.o.n, 3);
-    });
-
-    it('removes a property when it\'s value clone method returns undefined', () => {
-      const target = deserializer.deserialize(serializer.serialize({
-        o: {
-          clone() {}
+        class C2 {
         }
-      }));
+        const source = new C1();
+        source.c2 = new C2();
 
-      assert(!target.hasOwnProperty('o'));
+        const deserializerWithClasses = new Deserializer({classes: {C1, C2}});
+
+        const target = deserializerWithClasses.deserialize(serializer.serialize(sourceMapper(source)));
+
+
+        assert.strictEqual(Reflect.getPrototypeOf(target), C1.prototype);
+        assert.strictEqual(Reflect.getPrototypeOf(target.c2), C2.prototype);
+      });
     });
-
-    it('inner clone method receives serialize\'s second parameter', () => {
-      const clone = sinon.spy();
-
-      deserializer.deserialize(serializer.serialize({
-        o: {clone}
-      }, 1));
-
-      assert(clone.calledOnce);
-      assert(clone.calledWithExactly(1));
-    });
-  });
-
-  it('deserializer with extra classes', () => {
-    class C1 {}
-    class C2 {}
-    const source = new C1();
-    source.c2 = new C2();
-
-    const deserializerWithClasses = new Deserializer({classes: {C1, C2}});
-
-    const target = deserializerWithClasses.deserialize(serializer.serialize(source));
-
-
-    assert.strictEqual(Reflect.getPrototypeOf(target), C1.prototype);
-    assert.strictEqual(Reflect.getPrototypeOf(target.c2), C2.prototype);
   });
 });
